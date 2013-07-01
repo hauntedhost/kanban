530a3f9325a663c931e8835a10d2059faf179245
sean <somlor@gmail.com>
2013-06-30 18:03:44 -0700

    use initialize (instead of parse) in board and list to create lists and card collections

--- app/assets/javascripts/models/list.js
+++ app/assets/javascripts/models/list.js
@@ -1,6 +1,12 @@
 Kanban.Models.List = Backbone.Model.extend({
-  parse: function (data) {
-    data.cards = new Kanban.Collections.Cards(data.cards);
-    return data;
+  initialize: function () {
+    console.log("initialize list");
+    var cards = this.get("cards");
+    this.set({ cards: new Kanban.Collections.Cards(cards) });
   }
+  
+  // parse: function (data) {
+  //   data.cards = new Kanban.Collections.Cards(data.cards);
+  //   return data;
+  // }
 });
