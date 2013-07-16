b80fb1abc3a4b339141a29c7ad4914a1728ef604
sean <somlor@gmail.com>
2013-07-16 11:50:38 -0700

    asset precompilation fix for heroku

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,8 @@ module Kanban
 
     # Enable the asset pipeline
     config.assets.enabled = true
-
+		config.assets.initialize_on_precompile = false
+		
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
