c822afb9000db7ed90d09d11b7b78639fffacb10
sean <somlor@gmail.com>
2013-07-16 11:57:52 -0700

    fix seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,6 @@ Board.create([
   { :name => "Simpson Family Chores", :description => "Chores manager for Simpson family", :open => true, :created_at => "2013-07-16 18:29:08", :updated_at => "2013-07-16 18:29:08" }
 ], :without_protection => true )
 
-
-
-BoardActivity.create([
-  { :member_id => nil, :board_id => nil, :description => nil, :created_at => nil, :updated_at => nil }
-], :without_protection => true )
-
-
-
 BoardMember.create([
   { :board_id => 1, :member_id => 1, :admin => false, :created_at => "2013-07-16 18:29:08", :updated_at => "2013-07-16 18:29:08" },
   { :board_id => 1, :member_id => 2, :admin => false, :created_at => "2013-07-16 18:29:08", :updated_at => "2013-07-16 18:29:08" },
@@ -30,52 +22,36 @@ BoardMember.create([
   { :board_id => 4, :member_id => 8, :admin => false, :created_at => "2013-07-16 18:29:09", :updated_at => "2013-07-16 18:29:09" }
 ], :without_protection => true )
 
-
-
 Card.create([
   { :list_id => 8, :title => "saxophone practice", :description => nil, :due_date => nil, :open => true, :created_at => "2013-07-16 18:30:40", :updated_at => "2013-07-16 18:32:49", :position => 1, :assignee_id => 7 },
   { :list_id => 7, :title => "mow the lawn", :description => nil, :due_date => nil, :open => true, :created_at => "2013-07-16 18:33:07", :updated_at => "2013-07-16 18:33:11", :position => 1, :assignee_id => 4 },
-  { :list_id => 1, :title => "pass lists/cards with board", :description => nil, :due_date => nil, :open => true, :created_at => "2013-07-16 18:29:09", :updated_at => "2013-07-16 18:29:09", :position => 1, :assignee_id => 1 },
-  { :list_id => 3, :title => "add boards index", :description => nil, :due_date => nil, :open => true, :created_at => "2013-07-16 18:29:09", :updated_at => "2013-07-16 18:29:09", :position => 2, :assignee_id => 3 },
   { :list_id => 8, :title => "drink beer and watch tv", :description => nil, :due_date => nil, :open => true, :created_at => "2013-07-16 18:32:54", :updated_at => "2013-07-16 18:32:57", :position => 2, :assignee_id => 4 },
   { :list_id => 7, :title => "take out the trash", :description => nil, :due_date => nil, :open => true, :created_at => "2013-07-16 18:33:09", :updated_at => "2013-07-16 18:33:14", :position => 2, :assignee_id => 4 },
   { :list_id => 7, :title => "animal rights rally", :description => nil, :due_date => nil, :open => true, :created_at => "2013-07-16 18:35:22", :updated_at => "2013-07-16 18:35:26", :position => 3, :assignee_id => 7 },
-  { :list_id => 3, :title => "add boards show", :description => nil, :due_date => nil, :open => true, :created_at => "2013-07-16 18:29:09", :updated_at => "2013-07-16 18:29:09", :position => 3, :assignee_id => nil },
   { :list_id => 7, :title => "write itchy & scratchy complaint letter", :description => nil, :due_date => nil, :open => true, :created_at => "2013-07-16 18:37:09", :updated_at => "2013-07-16 18:38:58", :position => 4, :assignee_id => 5 },
-  { :list_id => 4, :title => "chains.cc", :description => nil, :due_date => nil, :open => true, :created_at => "2013-07-16 18:29:09", :updated_at => "2013-07-16 18:29:09", :position => 4, :assignee_id => nil },
   { :list_id => 9, :title => "do not have a cow", :description => nil, :due_date => nil, :open => true, :created_at => "2013-07-16 18:30:59", :updated_at => "2013-07-16 18:32:45", :position => 6, :assignee_id => 6 },
   { :list_id => 9, :title => "make pancakes!", :description => nil, :due_date => nil, :open => true, :created_at => "2013-07-16 18:31:06", :updated_at => "2013-07-16 18:32:43", :position => 7, :assignee_id => 5 },
-  { :list_id => 9, :title => "shoot mr. burns", :description => nil, :due_date => nil, :open => true, :created_at => "2013-07-16 18:32:32", :updated_at => "2013-07-16 18:32:39", :position => 8, :assignee_id => 8 }
+  { :list_id => 9, :title => "shoot mr. burns", :description => nil, :due_date => nil, :open => true, :created_at => "2013-07-16 18:32:32", :updated_at => "2013-07-16 18:32:39", :position => 8, :assignee_id => 8 },
+
+  { :list_id => 1, :title => "pass lists/cards with board", :description => nil, :due_date => nil, :open => true, :created_at => "2013-07-16 18:29:09", :updated_at => "2013-07-16 18:29:09", :position => 1, :assignee_id => 1 },
+  { :list_id => 3, :title => "add boards index", :description => nil, :due_date => nil, :open => true, :created_at => "2013-07-16 18:29:09", :updated_at => "2013-07-16 18:29:09", :position => 2, :assignee_id => 3 },
+  { :list_id => 3, :title => "add boards show", :description => nil, :due_date => nil, :open => true, :created_at => "2013-07-16 18:29:09", :updated_at => "2013-07-16 18:29:09", :position => 3, :assignee_id => nil },
+  { :list_id => 4, :title => "chains.cc", :description => nil, :due_date => nil, :open => true, :created_at => "2013-07-16 18:29:09", :updated_at => "2013-07-16 18:29:09", :position => 4, :assignee_id => nil }
 ], :without_protection => true )
-
-
-
-CardActivity.create([
-  { :member_id => nil, :card_id => nil, :description => nil, :created_at => nil, :updated_at => nil }
-], :without_protection => true )
-
-
 
 CardComment.create([
-  { :card_id => 14, :commenter_id => 6, :content => "¡ay, caramba! but, mom, this is my favorite show!", :created_at => "2013-07-16 18:45:07", :updated_at => "2013-07-16 18:45:07" },
+  { :card_id => 1, :commenter_id => 5, :content => "your playing sounds great lisa! :)", :created_at => "2013-07-16 18:43:27", :updated_at => "2013-07-16 18:43:27" },
+  { :card_id => 6, :commenter_id => 6, :content => "¡ay, caramba! but, mom, this is my favorite show!", :created_at => "2013-07-16 18:45:07", :updated_at => "2013-07-16 18:45:07" },
   { :card_id => 9, :commenter_id => 5, :content => "i know this was only an accident maggie. :(", :created_at => "2013-07-16 18:43:40", :updated_at => "2013-07-16 18:43:40" },
-  { :card_id => 6, :commenter_id => 5, :content => "your playing sounds great lisa! :)", :created_at => "2013-07-16 18:43:27", :updated_at => "2013-07-16 18:43:27" },
-  { :card_id => 12, :commenter_id => 5, :content => "homer, honey, the trash is overflowing, please take it out soon.", :created_at => "2013-07-16 18:42:58", :updated_at => "2013-07-16 18:42:58" },
+  { :card_id => 4, :commenter_id => 5, :content => "homer, honey, the trash is overflowing, please take it out soon.", :created_at => "2013-07-16 18:42:58", :updated_at => "2013-07-16 18:42:58" },
   { :card_id => 8, :commenter_id => 7, :content => "they were delicious!", :created_at => "2013-07-16 18:41:48", :updated_at => "2013-07-16 18:41:48" },
-  { :card_id => 13, :commenter_id => 7, :content => "please comment here if you would like to join me.", :created_at => "2013-07-16 18:41:16", :updated_at => "2013-07-16 18:41:16" },
+  { :card_id => 5, :commenter_id => 7, :content => "please comment here if you would like to join me.", :created_at => "2013-07-16 18:41:16", :updated_at => "2013-07-16 18:41:16" },
   { :card_id => 8, :commenter_id => 4, :content => "thanks marge. :)", :created_at => "2013-07-16 18:40:15", :updated_at => "2013-07-16 18:40:15" },
-  { :card_id => 2, :commenter_id => 1, :content => "nice", :created_at => "2013-07-16 18:29:09", :updated_at => "2013-07-16 18:29:09" },
-  { :card_id => 1, :commenter_id => 1, :content => "will do this soon", :created_at => "2013-07-16 18:29:09", :updated_at => "2013-07-16 18:29:09" },
-  { :card_id => 1, :commenter_id => 1, :content => "hello world", :created_at => "2013-07-16 18:29:09", :updated_at => "2013-07-16 18:29:09" }
+
+  { :card_id => 10, :commenter_id => 1, :content => "nice", :created_at => "2013-07-16 18:29:09", :updated_at => "2013-07-16 18:29:09" },
+  { :card_id => 10, :commenter_id => 1, :content => "will do this soon", :created_at => "2013-07-16 18:29:09", :updated_at => "2013-07-16 18:29:09" },
+  { :card_id => 10, :commenter_id => 1, :content => "hello world", :created_at => "2013-07-16 18:29:09", :updated_at => "2013-07-16 18:29:09" }
 ], :without_protection => true )
-
-
-
-CardMember.create([
-  { :card_id => nil, :member_id => nil, :created_at => nil, :updated_at => nil }
-], :without_protection => true )
-
-
 
 List.create([
   { :board_id => 1, :title => "todo", :open => true, :created_at => "2013-07-16 18:29:09", :updated_at => "2013-07-16 18:29:09", :position => 1 },
@@ -89,8 +65,6 @@ List.create([
   { :board_id => 4, :title => "Finished", :open => true, :created_at => "2013-07-16 18:30:18", :updated_at => "2013-07-16 18:30:18", :position => 9 }
 ], :without_protection => true )
 
-
-
 User.create([
   { :username => nil, :email => "somlor@gmail.com", :password_digest => "$2a$10$pG537CGeFX4MBSRYQfgk..eEL.9UhVQ1wvScsv/cb2HmF9EneSvre", :session_key => nil, :activation_key => nil, :bio => "Rails hacker", :created_at => "2013-07-16 18:29:07", :updated_at => "2013-07-16 18:29:07", :full_name => "Sean Omlor" },
   { :username => nil, :email => "sebastian@catmail.com", :password_digest => "$2a$10$L2nKfjwx.PEa2LEdud3jb.rDBc9BDu8BUNU5VAsGVdCkqPkSQMF4y", :session_key => nil, :activation_key => nil, :bio => "Strange cat", :created_at => "2013-07-16 18:29:07", :updated_at => "2013-07-16 18:29:07", :full_name => "Sebastian" },
@@ -101,5 +75,3 @@ User.create([
   { :username => nil, :email => "lisa2@sent.as", :password_digest => "$2a$10$o3R/LSeFncx2cIjpMVC4KuLZU7MP.RzEf92vYedoVrroWGTAtPbMi", :session_key => "_8OlOeY3C5xFm57eCuP7A4buNbgjVn5awkHV1AxBXnk", :activation_key => nil, :bio => "Lisa Simpson", :created_at => "2013-07-16 18:29:08", :updated_at => "2013-07-16 18:40:49", :full_name => "Lisa" },
   { :username => nil, :email => "maggie@sent.as", :password_digest => "$2a$10$8SLnv3a2vyCSyXCYgbgmNuSJz62S8R9zOWAnGVSeKHCmDkkJu/P3S", :session_key => nil, :activation_key => nil, :bio => "Maggie Simpson", :created_at => "2013-07-16 18:29:08", :updated_at => "2013-07-16 18:29:08", :full_name => "Maggie" }
 ], :without_protection => true )
-
-
