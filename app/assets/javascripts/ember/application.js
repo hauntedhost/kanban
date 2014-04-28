97e0c30b0ec630311145e91959df4c25b8c5c8c9
Sean Omlor <somlor@eml.cc>
2014-04-28 00:29:08 -0400

    upgrade to latest ember/ember-data

--- app/assets/javascripts/ember/application.js
+++ app/assets/javascripts/ember/application.js
@@ -1,6 +1,6 @@
 //= require handlebars
-//= require ember
-//= require ember-data
+//= require ember-1.6.0-beta.3+pre.2e4d67f6
+//= require ember-data-1.0.0-beta.7.f87cba88
 //= require_self
 //= require ./kanban
 
