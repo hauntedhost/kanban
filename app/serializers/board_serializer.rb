bc2cdab803cacda5485639a92835d34f78e91439
Sean Omlor <somlor@eml.cc>
2014-04-28 20:55:14 -0400

    fix serializers for board>lists and list>cards

--- app/serializers/board_serializer.rb
+++ app/serializers/board_serializer.rb
@@ -2,5 +2,5 @@ class BoardSerializer < ActiveModel::Serializer
   embed :ids, include: true
   attributes :id, :description, :name, :open
   has_many :members, key: :users, root: :users
-  has_many :lists
+  has_many :lists, key: :lists
 end
--- app/serializers/list_serializer.rb
+++ app/serializers/list_serializer.rb
@@ -2,5 +2,5 @@ class ListSerializer < ActiveModel::Serializer
   embed :ids, include: true
   attributes :id, :title, :open, :position
   has_one :board, root: :boards, key: :board
-  has_many :cards
+  has_many :cards, key: :cards
 end
