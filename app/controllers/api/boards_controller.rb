9b52e45785183f335d761ffc9bd757a08ddca3f2
Aaron Boushley <boushley@gmail.com>
2014-06-10 23:00:45 -0700

    Add create board functionality.

--- app/controllers/api/boards_controller.rb
+++ app/controllers/api/boards_controller.rb
@@ -10,6 +10,17 @@ module Api
       @board = current_user.boards.find(params[:id])
     end
 
+    def create
+      board = Board.new(params[:board])
+
+      if board.save
+        board.members << current_user
+        render json: board, status: :ok
+      else
+        render json: board.errors, status: :unprocessable_entity
+      end
+    end
+
     def update
     	board = current_user.boards.find(params[:id])
     	if board.update_attributes(params[:board])
