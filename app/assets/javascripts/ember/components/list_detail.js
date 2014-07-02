f06d9aa6541b1a83d418ca65eceddff0500d0421
Sean Omlor <somlor@eml.cc>
2014-07-02 00:15:16 -0400

    delete unused cruft js

--- app/assets/javascripts/ember/components/list_detail.js
+++
@@ -1,7 +0,0 @@
-// Kanban.ListDetailComponent = Ember.Component.extend({
-//   classNames: ['list'],
-//   attributeBindings: ['data-position'],
-//   'data-position': function() {
-//     return 'list_' + this.list.id;
-//   }.property()
-// });
